33437c10658d93ae20f9e774e4a552854d2d868a
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2025-03-16 12:48:53 +0100

    ready game

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
 'use strict';
 
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+const readline = require('node:readline');
+
 // Write your code here
+
+const quessNumbers = generateRandomNumber();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Enter your numbers', (numbers) => {
+  if (!checkIsValidUserInput(numbers)) {
+    rl.write('your numbers are invalid');
+    rl.close();
+  } else {
+    const result = getBullsAndCows(numbers, quessNumbers);
+
+    rl.write(`buls: ${result.bulls}; Cows: ${result.cows}\n`);
+    rl.close();
+  }
+});
